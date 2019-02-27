545afce51b37dcca2eb758c1652432c5bd41c145
Xiaoming Wang <40604687+wxiaom86@users.noreply.github.com>
2019-02-27 03:47:15 -0500

    Update test.py

--- new/test.py
+++ new/test.py
@@ -3,10 +3,10 @@ import numpy as np
 # define parameters
 # E1 = 1/2 w1^2 Q^2
 # E2 = 1/2 w2^2 (Q-d)^2 - dE
-w1_   =    0.01045   # eV
-w2_   =    0.01061
-d     =    9.0056    # sqrt(amu)*A
-dE    =    0.4219    # eV
+w1_   =    0.00760   # eV
+w2_   =    0.00868
+d     =    8.496    # sqrt(amu)*A
+dE    =    0.37    # eV
 # conversion units
 kb    =    8.6173303e-5  # eV/K          
 hbar  =    6.582119514e-16 # eV.s/rad
@@ -30,13 +30,8 @@ n = 2
 e = (n + 0.5)*w1_
 a = np.sqrt(2*e/w1**2)
 c = d - np.sqrt(2*(e + dE)/w2**2)
-l = 0.5*b*np.sqrt(w1**2*b**2-2*e) - e/w1*np.log(w1*np.sqrt(w1**2*b**2-2*e)+w1**2*b) - \
-    0.5*a*np.sqrt(w1**2*a**2-2*e) + e/w1*np.log(w1*np.sqrt(w1**2*a**2-2*e)+w1**2*a) 
-
-r = 0.5*(c-d)*np.sqrt(w2**2*(c-d)**2-2*(dE+e)) - \
-    0.5*(b-d)*np.sqrt(w2**2*(b-d)**2-2*(dE+e)) + \
-    (dE+e)/w2*np.log((np.sqrt(w2**2*(b-d)**2-2*(dE+e))+w2*(b-d))/(np.sqrt(w2**2*(c-d)**2-2*(dE+e))+w2*(c-d))) 
-
+l = b/2*np.sqrt(2*v-2*e) + e/w1*np.log(w1*a/(np.sqrt(2*v-2*e)+w1*b))
+r = (d-b)/2*np.sqrt(2*v-2*e)+(dE+e)/w2*np.log((np.sqrt(2*v-2*e)+w2*(b-d))/w2/(c-d))
 print "a,c,l,r:", a,c,l,r
 gamma = (l + r)/np.sqrt(ha2ev)*np.sqrt(amu2au)*a2b
 t = np.exp(-2*gamma)
